8bb962f960bd72b1678f95d8726a99c316e3bdb7
Renan Carvalho Manera <126332577+renancmanera@users.noreply.github.com>
2025-06-04 00:37:50 -0300

    Create thelastproblem.cpp

---
+++ Kattis/thelastproblem.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    string nome;
+    
+    getline(cin, nome);
+    
+    cout << "Thank you, " << nome << ", and farewell!" << endl;
+    
+   return 0;
+}
