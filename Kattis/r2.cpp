b2155902a3a07bec969594fb89115687195d6d45
Renan Carvalho Manera <126332577+renancmanera@users.noreply.github.com>
2025-06-04 00:22:08 -0300

    Create r2.cpp

---
+++ Kattis/r2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int n1, media;
+    
+    cin >> n1;
+    cin >> media;
+    
+    cout << 2 * media - n1 << endl;
+    
+   return 0;
+}
