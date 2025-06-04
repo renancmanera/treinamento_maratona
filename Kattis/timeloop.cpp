27dea301d100039c847036239d3ef3222560c513
Renan Carvalho Manera <126332577+renancmanera@users.noreply.github.com>
2025-06-04 00:45:08 -0300

    Create timeloop.cpp

---
+++ Kattis/timeloop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int contador, contagem;
+    
+    cin >> contador;
+    
+    for(contagem = 1; contagem < contador + 1; contagem++){
+        cout << contagem << " Abracadabra" << endl;
+    }
+    
+   return 0;
+}
