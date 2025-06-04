92fbd0443868699b9bc47c842925fe8bd15e985a
Renan Carvalho Manera <126332577+renancmanera@users.noreply.github.com>
2025-06-04 01:01:56 -0300

    Create 1124  –  Celebrity Jeopardy.cpp

---
+++ UVa/1124  –  Celebrity Jeopardy.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    string linha;
+    
+    while(getline(cin, linha)){
+        cout << linha << endl;
+    }
+    
+   return 0;
+}
